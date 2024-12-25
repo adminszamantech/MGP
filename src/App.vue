7394ba6beda99a2b719e3e7544e71891aebac3cc
Mortuza Ahmed <mortuza.ahmed@dp24.com>
2024-12-25 19:12:35 +0600

    Header top and slider

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   // imports
   import Header from '@/views/layouts/Header.vue';
   import Footer from '@/views/layouts/Footer.vue';
-
+  import HeaderTop from '@/views/components/home/HeaderTop.vue';
+  import Slider from '@/views/components/home/Slider.vue';
   // variables
 
   // methods
@@ -14,16 +15,8 @@
       <header>
         <Header/>
       </header>
-      <div class="header-menu">
-          <ul class="list-unstyled d-flex justify-content-between align-items-center px-3 py-2">
-            <li><a class="text-decoration-none text-white fs-5" href="">LUDO</a></li>
-            <li><a class="text-decoration-none text-white fs-5" href="">RUMMY</a></li>
-            <li><a class="text-decoration-none text-white fs-5" href="">POKER</a></li>
-            <li><a class="text-decoration-none text-white fs-5" href="">TEEN PATTI</a></li>
-            <li><a class="text-decoration-none text-white fs-5" href="">AVIATOR</a></li>
-            <li><a class="text-decoration-none text-white fs-5" href="">LIVE CASSINO</a></li>
-          </ul>
-      </div>
+      <HeaderTop/>
+      <Slider/>
       <main>
         <router-view />
       </main>
