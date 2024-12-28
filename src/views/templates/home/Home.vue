978cffc40f617f8c53ce308a9a43a9df2a6ce68f
Mortuza Ahmed <mortuza.ahmed@dp24.com>
2024-12-28 12:55:05 +0600

    Some Changes

--- src/views/templates/home/Home.vue
+++ src/views/templates/home/Home.vue
@@ -1,3 +1,10 @@
+<script setup>
+import Slider from '@/views/components/home/Slider.vue';
+import MenuCard from '@/views/components/home/MenuCard.vue';
+
+</script>
+
 <template>
-    
+    <Slider/>
+    <MenuCard/>
 </template>
